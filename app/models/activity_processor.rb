edfc708081bc58fb316300aba7c8ce3b4607daca
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2012-05-24 09:37:06 -0300

    the uri to remote evaluator was wrong

--- app/models/activity_processor.rb
+++ app/models/activity_processor.rb
@@ -5,7 +5,7 @@ class ActivityProcessor
     exercise = GnarusActivity::Exercise.find(params[:exercise_id])
     verify = exercise.content
 
-    uri = EvaluatorService::Config.remote_evaluator
+    uri = URI(EvaluatorService::Config.remote_evaluator + "/please")
     response = Net::HTTP.post_form(uri, code: params[:solution], verify: verify)
     is_correct = response.body == "true"
 
