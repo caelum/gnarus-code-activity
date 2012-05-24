d59ad0f0648d94726b293f060ae13d4d0a98a035
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2012-05-24 10:12:31 -0300

    correcting the configuration file
    better http req handling

--- app/models/activity_processor.rb
+++ app/models/activity_processor.rb
@@ -1,3 +1,5 @@
+#encoding: utf-8
+
 require 'net/http'
 
 class ActivityProcessor
@@ -5,9 +7,16 @@ class ActivityProcessor
     exercise = GnarusActivity::Exercise.find(params[:exercise_id])
     verify = exercise.content
 
-    uri = URI(EvaluatorService::Config.remote_evaluator + "/please")
-    response = Net::HTTP.post_form(uri, code: params[:solution], verify: verify)
-    is_correct = response.body == "true"
+    url = EvaluatorService::Config.remote_evaluator + "/please"
+    response = nil
+    is_correct = false
+    begin
+      req_params = {code: params[:solution], verify: verify}
+      response = Net::HTTP.post_form(URI.parse(url), req_params)
+      is_correct = response.is_a?(Net::HTTPSuccess) && response.body == "true"
+    rescue SystemCallError => e
+      flash[:error] = "Impossível validar o exercício"
+    end
 
     attempt.executions.create(
       solution: params[:solution],
